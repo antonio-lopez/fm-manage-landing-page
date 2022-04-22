38e2c23a7ba40d80cea2b42fc4f93ac0b1c0e09b
Antonio Lopez <pezlop202@gmail.com>
2022-04-21 17:20:16 -0700

    navbar and image assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <h1 className='text-sm'>yo</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
